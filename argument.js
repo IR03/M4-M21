6139a6ab0483ee9a643b6502fe5296d4742ca46a
IR03 <iftikharsakib@gmail.com>
2021-03-18 11:42:49 +0600

    1 project & some basic code

---
+++ argument.js
@@ -0,0 +1,8 @@
+function getFullName(firstName , lastName){
+    console.log(arguments);
+    //const fullName = arguments;   //firstName + ' ' + lastName;
+    //return fullName;
+}
+
+const name = getFullName('Hanif' , 'Poribohon' , 'Lalmiya');
+//console.log(name);
